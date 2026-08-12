c5d125e5c0867eb67f49b9ce710fdc849d0fdbf3
Thwin Htoo Aung <htoohtoo.mdy.mmr@gmail.com>
2026-08-12 17:59:28 +0700

    Initial Commit

---
+++ Little Hero/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Little Hero
+//
+//  Created by Thwin Htoo Aung on 12/8/2569 BE.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Little Hero/Little_HeroApp.swift
@@ -0,0 +1,17 @@
+//
+//  Little_HeroApp.swift
+//  Little Hero
+//
+//  Created by Thwin Htoo Aung on 12/8/2569 BE.
+//
+
+import SwiftUI
+
+@main
+struct Little_HeroApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
